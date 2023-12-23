af58c3f45bdf9d0c83d8d1cac5de3e16548d1988
MichalKleban <michal0kleban@gmail.com>
2023-12-23 15:22:40 +0100

    complite earth forcast

--- nasa-page/src/app/earth/earth.component.ts
+++ nasa-page/src/app/earth/earth.component.ts
@@ -9,8 +9,14 @@ declare function weather_data(): any;
   styleUrl: './earth.component.scss'
 })
 export class EarthComponent {
+  public date!: string;
+  public tem!: string;
+  public maxtem!: string;
+  public mintem!: string;
+  public humidity!: string;
+  public windSpeed!: string;
   constructor(){
-    document.body.style.backgroundImage = "url('assets/earth.jpg')";
+    document.body.style.backgroundImage = "url('assets/earth.jpeg')";
     document.body.style.backgroundPosition = "center center";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundAttachment = "fixed";
@@ -19,24 +25,27 @@ export class EarthComponent {
   ngOnDestroy(){
     document.body.style.backgroundImage = "none";
   }
-  weather_data(){
-   
-    const apiKey = "RMFD55BTFJSM7A5T6S84MMUPJ";
-    const location = ["kraków", "warszawa", "nowy jork"];
-    location.forEach(city =>{
-    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/warszawa?unitGroup=metric&include=days&key=RMFD55BTFJSM7A5T6S84MMUPJ&contentType=json", {
-        "method": "GET",
-        "headers": {
-        }
-        })
-      .then(response => response.json())
-      .then(data => {
-        const tem = data.days[0].windspeed;
-        const airq = data.days[1].windspeed;
 
-        console.log(tem , airq)
-  })
-  .catch(error => console.error("Błąd"))
-  })
+  weather_data(e: any){
+    
+    let choosen_city = e.target.value;
+    console.log(choosen_city);
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${choosen_city}?unitGroup=metric&include=days&key=RMFD55BTFJSM7A5T6S84MMUPJ&contentType=json`, {
+      "method": "GET",
+      "headers": {
+      }   
+    })
+    .then(response => response.json()) 
+    .then(data => {
+      this.date = data.days[0].datetime;
+      this.tem = data.days[0].temp;
+      this.maxtem = data.days[0].tempmax;
+      this.mintem = data.days[0].tempmin;
+      this.humidity = data.days[0].humidity;
+      this.windSpeed = data.days[0].windspeed;
+      console.log(choosen_city)
+    })
 }
 }
+
+  
